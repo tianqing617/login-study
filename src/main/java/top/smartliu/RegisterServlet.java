328c16c3fd0824099f55ce8b9346b0829bf2ff56
Tianqing Liu <liutq@hollycrm.com>
2023-02-24 11:34:01 +0800

    feat: 初始化注册servlet

---
+++ src/main/java/top/smartliu/RegisterServlet.java
@@ -0,0 +1,22 @@
+package top.smartliu;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "RegisterServlet", value = "/registerServlet")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        writer.write("success222");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
